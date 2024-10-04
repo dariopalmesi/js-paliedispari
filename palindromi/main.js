5d712515759c6049c49430cc770f1c2174b9ac93
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-04 17:05:20 +0200

    upgrade palindromo commit

--- palindromi/main.js
+++ palindromi/main.js
@@ -2,15 +2,21 @@
 
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-
+prompt('inserisci una parola')
 
 function log_user_promt(str) {
+    if (str == str) {
+        console.log('è palindromo');
 
+    }else {
+        console.log('non è palindromo');
+        
+    }
     console.log(str);
-
+    
     return str == str.split('').reverse().join('');
 
 
 }
 
-log_user_promt('inserisci una parola')
+log_user_promt()
